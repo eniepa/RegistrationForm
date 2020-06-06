9e00f8b5abb26e2b29a5e7dd1bf8a99b792893af
eniepa <apeine.k@gmail.com>
2020-06-06 18:55:42 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == string.Empty || textBox2.Text == string.Empty || textBox3.Text == string.Empty)
+            {
+                MessageBox.Show("Enter your data");
+                return;
+            }
+            if (textBox2.Text != textBox3.Text)
+            {
+                textBox2.BackColor = Color.Red;
+                textBox3.BackColor = Color.Red;
+                MessageBox.Show("Passwords do not match");
+                return;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox2.PasswordChar = '\0';
+                textBox3.PasswordChar = '\0';
+            }
+            else
+            {
+                textBox2.PasswordChar = '*';
+                textBox3.PasswordChar = '*';
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = false;
+                this.BackColor = Color.White;
+                foreach (Control c in this.Controls)
+                {
+                    c.BackColor = Color.White;
+                    c.ForeColor = Color.Black;
+                }
+            }
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton1.Checked = false;
+                this.BackColor = Color.Black;
+                foreach (Control c in this.Controls)
+                {
+                        c.BackColor = Color.Black;
+                        c.ForeColor = Color.White;
+                }
+            }
+            
+        }
+    }
+}
